95b6e1ccb68b31baded4ecc69c0640f15e4e1b56
KamilKojs <kamil.kojs@gmail.com>
2022-11-13 19:46:31 +0100

    files renamed

---
+++ src/rename_files.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from os import listdir
+from os.path import isfile, join
+import os
+import pandas as pd
+
+def main():
+    dir_path = "/Users/kamil/Documents/Dokumenty/ITU/Sem1/Data_in_the_wild/project/DITW_project/data/twitter/wolf_of_wall_street"
+    all_files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+    all_files = sorted(all_files)
+
+    for file in all_files:
+        file_name = os.path.basename(file)
+        if file.startswith('start'):
+            df = pd.read_csv(f"{dir_path}/{file}")
+            new_file_name = file.replace(",", "-")
+            new_file_name = file.replace(" ", "-")
+            df.to_csv(f"{dir_path}/{new_file_name}")
+
+
+if __name__ == "__main__":
+    main()
