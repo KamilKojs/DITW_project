09940743a5f31e227cbc5d0c966f1998838132e3
jorgedelpozolerida <jorgedelpozolerida@gmail.com>
2022-11-16 20:47:21 +0100

    modified:   data/movies_with_scores.csv 	script for calculating scores finished:   src/calculate_moviescores.py

--- src/calculate_moviescores.py
+++ src/calculate_moviescores.py
@@ -19,50 +19,78 @@ map_folder2movieid_dict = {
     6: 'wolf_of_wall_street'
 }
 
-# TO DO:
-# - Compute sentiment score
-# Change fixed filepaths to variables once known the format
-# save output in csv with scores for each film
-# Create another python notebook
-
 
 def locate_sentimentcsv(movie_id, map_folder2movieid_dict):
-
+    '''
+    Helper function to get csv filepath of a certain movie
+    '''
+    # Get folder name from mapping dictionary by id
     folder_name = map_folder2movieid_dict[movie_id]
+
+    # Get the csv filepath by pattern matching inside folder
     csvfile_path = glob.glob(os.path.join(project_path, 'data', 'twitter',
                                           folder_name, '*_sentiment.csv'))[0]
 
     return csvfile_path
 
 
-def calculate_sentiment_score(movie_id, map_folder2movieid_dict, sentiment_cols=['negative', 'neutral', 'positive']):
+def calculate_sentiment_score(movie_id, map_folder2movieid_dict,
+                              sentiment_cols=['negative', 'neutral', 'positive']):
+    '''
+    For each movie, calculate certain metrics related to sentiment based on model output.
+    Returns a list of values ot be added as columns
+    '''
 
-    # Get csv file for each movie
+    # Get csv file for movie and read
     csv_filepath = locate_sentimentcsv(movie_id,  map_folder2movieid_dict)
 
     sentiment_data = pd.read_csv(
         csv_filepath, usecols=sentiment_cols, sep="\t")
 
-    # Calculation of sentiment score
-    n_tweets = sentiment_data.shape[0]
-    sentiment_score = (sentiment_data.sum()).round(decimals=2)
+    # ----------- Calculation of different sentiment metrics -------------------
 
-    sentiment_score['total_score'] = (sentiment_score['negative']*-1 +
-                                      sentiment_score['neutral']*0 +
-                                      sentiment_score['positive']*1) / n_tweets
+    n_tweets = int(sentiment_data.shape[0])
 
-    sentiment_score['n_tweets'] = int(n_tweets)
+    # 1-Get how many tweets there are per cattegory in percentage
+    data_percent = sentiment_data.copy(deep=True).rename(columns={
+        'negative': 'perc_negative',
+        'neutral': 'perc_neutral',
+        'positive': 'perc_positive'
+    })
 
-    return sentiment_score[['total_score', 'n_tweets']]
+    data_percent['overall_sentiment'] = data_percent.apply(
+        (lambda row: row.index[np.argmax(row)]),
+        axis=1)  # select most probable one as the choice of the model
+
+    data_percent = (data_percent.groupby(
+        'overall_sentiment').size()*100/n_tweets).round(decimals=2)
+
+    # 2-Get the average probability for each cattegory
+    data_scores_mean = sentiment_data.copy(deep=True).rename(columns={
+        'negative': 'neg_score_avg',
+        'neutral': 'neutr_score_avg',
+        'positive': 'pos_score_avg'
+    }).mean().round(decimals=2)
+
+    # 3-Get a combined score weighting the different scores of the model
+    weighted_score_data = sentiment_data.copy(deep=True).sum()
+
+    sentiment_score = ((weighted_score_data['negative']*-1 +
+                       weighted_score_data['neutral']*0 +
+                       weighted_score_data['positive']*1)/n_tweets).round(decimals=4)
+
+    data_out = pd.concat([data_percent, data_scores_mean,
+                         pd.Series([sentiment_score, n_tweets], index=['sentiment_score', 'n_tweets'])])
+
+    return data_out
 
 
 def add_diversityscore(movies, actors):
     '''
-    Adds column related to diversity score to movies dataframe based on the presence of the presence
-    of non-white actors in their cast, given by a boolean column in actors dataframe
-
+    Adds columns related to diversity score to movies dataframe based on the presence of the presence
+    of non-white actors in their cast, given by a boolean column 'not_white' in actors dataframe
     '''
-
+    # Calculate diversity score as proportion of non-white actor in the main cast
     scores = (actors
               .groupby('movie_title').agg(
                   n_nonwhite=pd.NamedAgg(
@@ -74,16 +102,23 @@ def add_diversityscore(movies, actors):
               )
               )
     movies_withscore = (movies
-                        .merge(scores, left_on=['film_name'], right_on=['movie_title']))
+                        .merge(scores, left_on=['film_name'],
+                               right_on=['movie_title']))
 
     return movies_withscore
 
 
 def add_sentimentscore(movies, map_folder2movieid_dict):
+    '''
+    Adds columns related to sentiment score to movies dataframe based on the output
+    of the sentiment analysis model applied to tweets
+    '''
 
     # Calculate sentiment score for each
     movies_scores = movies.apply(
-        lambda row: calculate_sentiment_score(row['id'],  map_folder2movieid_dict), axis=1, result_type='expand')
+        lambda row: calculate_sentiment_score(
+            row['id'],  map_folder2movieid_dict),
+        axis=1, result_type='expand')
     movies = pd.concat([movies, movies_scores], axis='columns')
     return movies
 
@@ -101,7 +136,8 @@ def main():
 
     # read metadata from scraping
     scraped_data = (pd.read_json(
-        os.path.join(project_path, 'data', 'metadata', 'movies_metadata.json'), encoding='latin-1')
+        os.path.join(project_path, 'data', 'metadata', 'movies_metadata.json'),
+        encoding='latin-1')
         .T
     )
 
